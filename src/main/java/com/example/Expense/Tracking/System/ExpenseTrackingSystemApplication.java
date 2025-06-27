a3ee5201dc65c2dc60f0ed675769d9cb0f54ca6d
rocky2560 <sudipmanandhar2560@gmail.com>
2025-06-27 15:07:09 +0100

    first commit

---
+++ src/main/java/com/example/Expense/Tracking/System/ExpenseTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Expense.Tracking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackingSystemApplication.class, args);
+	}
+
+}
